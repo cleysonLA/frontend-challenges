94182e393bcd21ea7702fac60ed44b7b8edeaa4a
cleyson <clbbdev@gmail.com>
2021-01-03 16:25:22 -0300

    load fonts from assets

--- packages/web/src/styles/global.js
+++ packages/web/src/styles/global.js
@@ -1,15 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
+import RubikBold from '../assets/Rubik-Bold.ttf';
+import RubikMedium from '../assets/Rubik-Medium.ttf';
+import RubikRegular from '../assets/Rubik-Regular.ttf';
+
 export default createGlobalStyle`
+    @font-face {
+        font-family: 'Rubik';
+        src: url(${RubikBold});
+        src: url(${RubikMedium});
+        src: url(${RubikRegular});
+    }
+
     * {
         margin: 0;
         padding: 0;
         outline: 0;
         box-sizing: border-box;
     }
-    
+
     body {
-        font-family: 'Rubik', Arial, Helvetica, sans-serif;
+        font-family: 'Rubik', Arial, sans-serif;
         text-rendering: optimizeLegibility !important;
         -webkit-font-smoothing: antialiased;
     }
