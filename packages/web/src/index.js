b4d7944d6948f257028e59ba4349381853b54c60
cleyson <clbbdev@gmail.com>
2021-01-04 11:24:36 -0300

    add router

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
 
 import { ApolloProvider } from '@apollo/client';
 import api from './services/api';
 
+import App from './components/App';
+import Home from './pages/Home';
+
 ReactDOM.render(
   <React.StrictMode>
       <ApolloProvider client={api}>
-        <App />
+        <App>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </Router>
+        </App>
       </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
