7989ae0ee5084a137ee83b4bac2126253206a055
cleyson <clbbdev@gmail.com>
2021-01-08 12:36:53 -0300

    add priceFormatter function at utils

--- packages/web/src/pages/Home/index.js
+++ packages/web/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { useLazyQuery, gql } from "@apollo/client";
 import ProductPreviewCard from '../../components/ProductPreviewCard';
 import { Link } from 'react-router-dom';
 import { ProductsContainer, HomeTitle } from './styles';
+import priceFormatter from '../../utils/priceFormatter';
  
 const GET_PRODUCTS = gql`
     query ($page: Int, $perPage: Int) {
@@ -29,13 +30,6 @@ function Home() {
             handleProducts(allSkus);
         }
     });
-
-    function priceFormatter(price) {
-        return new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(price);
-    }
 
     function handleProducts(productsPerPage) {
         const productsFormatted = productsPerPage.reduce((productsReduced, product) => {
